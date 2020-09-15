eb1113a87a37b3146f810591fa267458bd6d3bf4
hguruman <hguruman@gmail.com>
2020-09-15 20:14:54 +0400

    Add spec for pairbot

---
+++ spec/pairbot_spec.rb
@@ -0,0 +1,16 @@
+# require_relative '../lib/bot.rb'
+
+# describe PairBot do
+#     context "When testing the #publishMessage method" do 
+#         it "sends a message to the slack channel given" do
+            
+#             #expect(sent).to eq "hello"
+#         end
+#     end
+# end
+
+# pairb = PairBot.new
+# channel = '#general'
+# text = "hello"
+
+# sent = pairb.publishMessage(channel, text)
