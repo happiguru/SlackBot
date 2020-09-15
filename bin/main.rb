9346f9f535db0c75d391ffdaff0f6d41a1d38308
hguruman <hguruman@gmail.com>
2020-09-15 19:51:18 +0400

    Add main file and instantiate a bot instance and call its methods

---
+++ bin/main.rb
@@ -0,0 +1,12 @@
+require '../lib/require.rb'
+require '../lib/bot.rb'
+
+pair = PairBot.new
+channel = '#general'
+text = "Welcome to Focus Team! Glad to have you! 
+Please note that PairBot will keep randomly pairing a team of two to keep you focus"
+pair.publishMessage(channel, text)
+
+#pair.verifyChannel
+
+pair.fetchUsers
